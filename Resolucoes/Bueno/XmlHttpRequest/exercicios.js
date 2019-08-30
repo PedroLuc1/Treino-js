461d672da475c95f97a3eab03c85036a592ff5bf
Gabriel Bueno <gabrielbueno99@outlook.com>
2019-08-30 16:58:23 -0300

    Continuação dos exercícios

--- Resolucoes/Bueno/XmlHttpRequest/exercicios.js
+++ Resolucoes/Bueno/XmlHttpRequest/exercicios.js
@@ -13,6 +13,7 @@ function getArtistas() {
 		if (http.readyState === XMLHttpRequest.DONE && http.status === 200) {
 			artistas = JSON.parse(http.responseText);
 
+			console.log("---> Lista de artistas: ");
 			console.log(artistas);
 		}
 	};
@@ -30,6 +31,7 @@ function getArtista(id) {
 		if (http.readyState === XMLHttpRequest.DONE && http.status === 200) {
 			artista = JSON.parse(http.responseText);
 
+			console.log("---> Artista por Id:");
 			console.log(artista);
 		}
 	};
@@ -37,5 +39,70 @@ function getArtista(id) {
 	http.send();
 }
 
-//getArtistas();
+/* Ex 3 */
+function createArtista(novoArtista) {
+	const http = new XMLHttpRequest();
+
+	http.open("POST", `${API_URL}/artistas`, true);
+	http.setRequestHeader("Content-Type: application/json");
+
+	http.onreadystatechange = function() {
+		if (this.readyState === XMLHttpRequest.DONE && http.status === 200) {
+			artista = JSON.parse(this.responseText);
+
+			console.log("---> Criando artista: ");
+			console.log(artista);
+		}
+	};
+
+	http.send(novoArtista);
+}
+
+/* Ex 4 */
+function updateArtista(id, novoArtista) {
+	const http = new XMLHttpRequest();
+
+	http.open("PUT", `${API_URL}/artistas/${id}`, true);
+
+	http.onreadystatechange = () => {
+		if (http.readyState === XMLHttpRequest.DONE && http.status === 200) {
+			artista = JSON.parse(http.responseText);
+
+			console.log("---> Editando o artista: ");
+			console.log(artista);
+		}
+	};
+
+	http.send(novoArtista);
+}
+
+/* Ex 5 */
+function deleteArtista(id) {
+	const http = new XMLHttpRequest();
+
+	http.open("DELETE", `${API_URL}/artistas/${id}`, true);
+
+	http.onreadystatechange = () => {
+		if (http.readyState === XMLHttpRequest.DONE && http.status === 200) {
+			console.log("Artista desativado com sucesso.");
+		}
+	};
+
+	http.send();
+}
+
+
+getArtistas();
 getArtista(1);
+
+createArtista({ 
+	nome: "Sudden Death", 
+	pais: "Não lembro" 
+});
+
+updateArtista(2, { 
+	nome: "Perfume Azul Do Sol", 
+	pais: "Brasil" 
+});
+
+deleteArtista(9);
